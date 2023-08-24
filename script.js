0002391dd287b975b3afd1807a561f00f250e68a
SupritD <supritdagade77@gmail.com>
2023-08-24 23:18:03 +0530

    video added and style added of sec01 to sec06

--- script.js
+++ script.js
@@ -1,35 +1,38 @@
 // alert("hey man!")
 
+// locomotive js code from github
+// const scroll = new LocomotiveScroll({
+//     el: document.querySelector('#main'),
+//     smooth: true
+// });
 
+// function for hamburger button
 const active_hamburger = () =>{
-    document.getElementById("hamburger").classList.toggle("active");
-    // document.querySelector("#hamburger").classList.toggle("active")
+    // document.getElementById("hamburger").classList.toggle("active");
+    document.querySelector("#hamburger").classList.toggle("active")
 }
 let a = 0;
 
+// function for preloader
 const time = () =>{
     setInterval(() => {
-
        a = a + Math.floor(Math.random()*20);
-    //    console.log(a); 
     if(a < 100){
-        document.querySelector("#loader h1").innerHTML = a + "%"
+        document.querySelector("#loader h1").innerHTML = a + "%";
     }
     else if(a >= 100){
         a = 100;
-        document.querySelector("#loader h1").innerHTML = a + "%"
-        // clearInterval(time);
+        document.querySelector("#loader h1").innerHTML = a + "%";
     }
     }, 150);
     
 }
 
+// gsap code
 var tl = gsap.timeline();
-
 
 tl.to("#loader h1",{
     delay: 1,
-    // opacity:0,
     duration:1,
     onStart:time()
 })
